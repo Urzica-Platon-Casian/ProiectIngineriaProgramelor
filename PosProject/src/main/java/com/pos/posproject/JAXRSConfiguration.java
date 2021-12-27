1ef70481c391bdcdce5b9bcf2b58e83201ab2cce
Casian Urzică-Platon <upcasian@yahoo.com>
2021-12-27 21:27:33 +0200

    new-project
    Created new project since the last one had issues

--- PosProject/src/main/java/com/pos/posproject/JAXRSConfiguration.java
+++ PosProject/src/main/java/com/pos/posproject/JAXRSConfiguration.java
@@ -1,9 +1,13 @@
-package com.pos.proiectpos;
+package com.pos.posproject;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
-
+    
 }
--- ProiectPos/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.pos/ProiectPos .
-call docker rm -f ProiectPos
-call docker run -d -p 9080:9080 -p 9443:9443 --name ProiectPos com.pos/ProiectPos
