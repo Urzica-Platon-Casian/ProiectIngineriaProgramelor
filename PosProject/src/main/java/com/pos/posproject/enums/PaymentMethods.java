710088b2ecc55be371db8ddfd5efff22888f359e
andreea-dumitrascu <72592844+andreea-dumitrascu@users.noreply.github.com>
2022-01-04 18:46:18 +0200

    modify the enum of the payment mathod

--- PosProject/src/main/java/com/pos/posproject/enums/PaymentMethods.java
+++ PosProject/src/main/java/com/pos/posproject/enums/PaymentMethods.java
@@ -5,17 +5,5 @@ package com.pos.posproject.enums;
  * @author Rori
  */
 public enum PaymentMethods {
-    CARD(0),
-    CASH(1);
-    
-    private final int id;
-
-    private PaymentMethods(int id) {
-        this.id = id;
-    }
-    
-    public int getValue()
-    {
-        return id;
-    }
+    CARD,CASH;
 }
