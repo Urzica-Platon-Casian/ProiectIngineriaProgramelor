165afca84e4a611e5cc53348a640ab48d06578a2
andreea-dumitrascu <72592844+andreea-dumitrascu@users.noreply.github.com>
2022-01-04 18:44:28 +0200

    create entity for sale

---
+++ PosProject/src/main/java/com/pos/posproject/entity/Sale.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pos.posproject.entity;
+
+import com.pos.posproject.enums.SaleStatus;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.json.bind.annotation.JsonbTransient;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author Rori
+ */
+@Entity
+public class Sale implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    private int cashierId;
+    private Date date;
+    private SaleStatus status;
+    
+    @OneToOne
+    @JoinColumn(name = "PAYMENT_KEY")
+    Payment payment;
+    
+    @JsonbTransient
+    @OneToMany(mappedBy = "sale")
+    private Collection<LineItem> saleProducts;
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getCashierId() {
+        return cashierId;
+    }
+
+    public void setCashierId(int cashierId) {
+        this.cashierId = cashierId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Collection<LineItem> getSaleProducts() {
+        return saleProducts;
+    }
+
+    public void setSaleProducts(Collection<LineItem> saleProducts) {
+        this.saleProducts = saleProducts;
+    }
+
+    public SaleStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(SaleStatus status) {
+        this.status = status;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Sale)) {
+            return false;
+        }
+        Sale other = (Sale) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.pos.posproject.entity.Sale[ id=" + id + " ]";
+    }
+    
+}
