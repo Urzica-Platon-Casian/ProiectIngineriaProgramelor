eb7e1fd63db265370ee3821727db4695d8b01497
Casian Urzică-Platon <upcasian@yahoo.com>
2022-01-05 22:34:23 +0200

    Create Sale servlet for cashier

---
+++ PosProject/src/main/java/com/pos/posproject/servlet/cashier/Sale.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pos.posproject.servlet.cashier;
+
+import com.pos.posproject.common.ProductDetails;
+import com.pos.posproject.ejb.ProductBean;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author upcas
+ */
+@WebServlet(name = "Sale", urlPatterns = {"/Sale"})
+public class Sale extends HttpServlet {
+
+        
+    @Inject
+    private ProductBean productBean;
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Sale</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Sale at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        List<ProductDetails> products = productBean.getAllProducts();
+        request.setAttribute("products", products);
+        request.getRequestDispatcher("/WEB-INF/pages/cashier/salePage.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
